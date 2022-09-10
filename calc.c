4f6e811334551408ec855b118c307fc40266c889
Arkadeep Acharya <acharyarka17@gmail.com>
2022-09-10 18:28:26 +0530

    Intial commit

---
+++ calc.c
@@ -0,0 +1,30 @@
+#include <stdio.h>  
+int main()  
+{  
+    
+    char opt;  
+    int n1, n2;   
+    float res;  
+    printf (" Choose an operator(+, -, *, /) to perform the operation in C Calculator \n ");  
+    scanf ("%c", &opt); 
+    if (opt == '/' )  
+    {  
+        printf ("Division selected");  
+    }  
+    else if (opt == '*')  
+    {  
+        printf ("Multiplication selected");  
+     }  
+       
+    else if (opt == '-')  
+    {  
+        printf ("Subtraction selected");  
+     }  
+        else if (opt == '+')  
+    {  
+        printf ("Addition selected ");  
+     }     
+    printf (" \n Enter the first number: ");  
+    scanf(" %d", &n1); // take fist number  
+    printf (" Enter the second number: ");  
+    scanf (" %d", &n2); // take second number  
