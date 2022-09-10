de2cc708527cd0c18f8ff978579afe77a07516f8
Arkadeep Acharya <acharyarka17@gmail.com>
2022-09-10 18:56:34 +0530

    Bubble sort

--- sort.c
+++ sort.c
@@ -16,4 +16,18 @@ void insertionSort(int arr[], int n)
         arr[j + 1] = key;
     }
 }
+void swap(int *xp, int *yp)
+{
+int temp = *xp;
+*xp = *yp;
+*yp = temp;
+}
 
+void bubblesort(int arr[],int n){
+    int i, j;
+for (i = 0; i < n-1; i++)
+for (j = 0; j < n-i-1; j++)
+if (arr[j] > arr[j+1])
+swap(&arr[j], &arr[j+1]);
+    
+}
